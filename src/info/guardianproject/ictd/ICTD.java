51343be755488753d95708223c0a7cef939f71ff
harlo <LvH@VIDEOCLASH.local>
2013-08-30 14:08:16 -0400

    ok, done-ish.

--- src/info/guardianproject/ictd/ICTD.java
+++ src/info/guardianproject/ictd/ICTD.java
@@ -1,6 +1,7 @@
 package info.guardianproject.ictd;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -14,6 +15,7 @@ import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.zip.GZIPOutputStream;
 
 import org.spongycastle.openpgp.PGPException;
 import org.spongycastle.openpgp.PGPPublicKey;
@@ -27,6 +29,9 @@ import android.content.Context;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.pm.Signature;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.util.Base64;
 import android.util.Log;
 
@@ -38,9 +43,10 @@ public class ICTD extends Model implements Serializable {
 	public String organizationFingerprint = null;
 
 	public byte[] publicKey = null;
+	public byte[] organizationIcon = null;
 
 	public List<IRepository> repositories = new ArrayList<IRepository>();
-	public List<String> forms = new ArrayList<String>();
+	public List<byte[]> forms = new ArrayList<byte[]>();
 
 	private Context c;
 	private final static String LOG = "*********************** ICTD Utility ***********************";
@@ -73,7 +79,7 @@ public class ICTD extends Model implements Serializable {
 				} catch (NoSuchAlgorithmException e) {
 					Log.e(LOG, e.toString());
 					e.printStackTrace();
-					break;
+					return;
 				}
 			}
 			
@@ -82,9 +88,52 @@ public class ICTD extends Model implements Serializable {
 		} catch (NameNotFoundException e) {
 			Log.e(LOG, e.toString());
 			e.printStackTrace();
+			
+			return;
 		}
+		
+		try {
+			Drawable d = this.c.getPackageManager().getApplicationIcon(this.c.getPackageName());
+			Bitmap b = ((BitmapDrawable) d).getBitmap();
+			
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			b.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+			
+			baos.flush();
+			b.recycle();
+			d = null;
+			
+			this.organizationIcon = Base64.encode(GZIP(baos.toByteArray()), Base64.DEFAULT);
+			baos.close();			
+			
+		} catch (NameNotFoundException e) {
+			Log.e(LOG, e.toString());
+			e.printStackTrace();
+		} catch (IOException e) {
+			Log.e(LOG, e.toString());
+			e.printStackTrace();
+		}
+		
 	}
 	
+	private static byte[] GZIP(byte[] bytes) {
+		try {
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			GZIPOutputStream gos = new GZIPOutputStream(baos);
+			gos.write(bytes);
+			gos.flush();
+			gos.close();
+			
+			return baos.toByteArray();
+		} catch (IOException e) {
+			Log.e(LOG, e.toString());
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+	
+	@SuppressWarnings("unchecked")
 	private static String getFingerprint(byte[] keyBlock) throws IOException, PGPException {
 		PGPPublicKeyRingCollection keyringCol = new PGPPublicKeyRingCollection(PGPUtil.getDecoderStream(new ByteArrayInputStream(Base64.decode(keyBlock, Base64.DEFAULT))));
 		PGPPublicKey key = null;
@@ -119,8 +168,7 @@ public class ICTD extends Model implements Serializable {
 		try {
 			organizationFingerprint = getFingerprint(this.publicKey);
 			if(organizationFingerprint != null) {
-				// TODO: GZIPPED?
-				this.publicKey = Base64.encode(publicKey, Base64.DEFAULT);
+				this.publicKey = Base64.encode(GZIP(publicKey), Base64.DEFAULT);
 			}
 		} catch (IOException e) {
 			Log.e(LOG, e.toString());
@@ -140,6 +188,7 @@ public class ICTD extends Model implements Serializable {
 				byte[] buf = new byte[fis.available()];
 				fis.read(buf);
 				setPublicKey(buf);
+				fis.close();
 			}
 		} catch (FileNotFoundException e) {
 			Log.e(LOG, e.toString());
@@ -149,6 +198,29 @@ public class ICTD extends Model implements Serializable {
 			e.printStackTrace();
 		}
 		
+	}
+	
+	public void setIcon(String iconPath) {
+		try {
+			FileInputStream fis = c.openFileInput(iconPath);
+			if(fis.available() > 0) {
+				byte[] buf = new byte[fis.available()];
+				fis.read(buf);
+				setIcon(buf);
+				fis.close();
+			}
+		} catch(FileNotFoundException e) {
+			Log.e(LOG, e.toString());
+			e.printStackTrace();
+		} catch(IOException e) {
+			Log.e(LOG, e.toString());
+			e.printStackTrace();
+		}
+	}
+	
+	public void setIcon(byte[] icon) {
+		this.organizationIcon = Base64.encode(GZIP(icon), Base64.DEFAULT);
+		icon = null;
 	}
 	
 	public void addRespository(IRepository repository) {
@@ -166,13 +238,33 @@ public class ICTD extends Model implements Serializable {
 		}
 	}
 	
-	public void addForm(String form) {
-		// TODO: GZIPPED?
-		form = Base64.encodeToString(form.getBytes(), Base64.DEFAULT);
+	public void addForm(String pathToForm) {
+		try {
+			FileInputStream fis = c.openFileInput(pathToForm);
+			if(fis.available() > 0) {
+				byte[] buf = new byte[fis.available()];
+				fis.read(buf);
+				addForm(buf);
+				fis.close();
+			}
+		} catch(FileNotFoundException e) {
+			Log.e(LOG, e.toString());
+			e.printStackTrace();
+		} catch(IOException e) {
+			Log.e(LOG, e.toString());
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void addForm(byte[] form) {
+		form = Base64.encode(GZIP(form), Base64.DEFAULT);
 		
 		if(!forms.contains(form)) {
 			forms.add(form);
 		}
+		
+		form = null;
 	}
 
 	public class IRepository extends Model implements Serializable {
